5142a05822aed5cf14e7ebf487f74f7929d4f896
pavel <pavel@Ubuntu.myguest.virtualbox.org>
2022-12-19 22:42:18 +0200

    init commit

---
+++ src/main/java/com/example/uaplanes/UaplanesApplication.java
@@ -0,0 +1,13 @@
+package com.example.uaplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaplanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UaplanesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uaplanes/UaplanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uaplanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaplanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
