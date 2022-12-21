20769f6b7ae98703fee25bd2ba4d41bf37f54e88
qeqoos <foxthesaw@gmail.com>
2022-12-22 00:16:22 +0200

    added delete method, added id iteration

--- src/main/java/com/example/uaplanes/Controller.java
+++ src/main/java/com/example/uaplanes/Controller.java
@@ -8,17 +8,16 @@ import java.util.List;
 public class Controller {
 
     private static List<Flight> flights = new ArrayList<>();
+    private static Integer currentId = 1;
     static {
-        Flight f1 = new Flight();
-        Flight f2 = new Flight();
-        flights.add(f1);
-        flights.add(f2);
+        Flight f = new Flight();
+        flights.add(f);
+        currentId += f.getId();
     }
 
     @GetMapping("/api/flights")
     @ResponseBody
     public String getAllFlights() {
-//        flightList.setFlights(flights);
         String response = "";
         for (Flight f:flights) {
             response += f.toString();
@@ -29,13 +28,28 @@ public class Controller {
 
     @GetMapping("/api/flights/{id}")
     public String getFlightById(@PathVariable int id) {
-        return flights.get(id).toString();
+        return flights.get(id - 1).toString();
     }
 
     @PostMapping("/api/flights/add")
     public String addFlight() {
         Flight f = new Flight();
+        f.id = currentId;
+        f.flightNumber = 123;
+        f.price = 13.7;
+        f.creationDate = "21.12.22 - 16:02";
+        f.departure = "Alaska";
+        f.arrival = "Stockholm";
+        f.departureDateTime = "23.12.22 - 22:30";
+        f.arrivalDateTime = "24.12.22 - 10:00";
         flights.add(f);
+        currentId += 1;
         return "Added flight: " + f.toString();
+    }
+
+    @DeleteMapping("/api/flights/{id}")
+    public String deleteFlightById(@PathVariable int id) {
+        flights.removeIf(obj -> obj.getId() == id);
+        return "Deleted flight with id " + id;
     }
 }
--- src/main/java/com/example/uaplanes/Flight.java
+++ src/main/java/com/example/uaplanes/Flight.java
@@ -10,6 +10,70 @@ public class Flight {
     String departureDateTime;
     String arrivalDateTime;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(int flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public String getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(String departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public String getArrivalDateTime() {
+        return arrivalDateTime;
+    }
+
+    public void setArrivalDateTime(String arrivalDateTime) {
+        this.arrivalDateTime = arrivalDateTime;
+    }
+
     public Flight() {
         id = 1;
         flightNumber = 228;
@@ -19,17 +83,6 @@ public class Flight {
         arrival = "San-Paulo";
         departureDateTime = "22.12.22 - 6:30";
         arrivalDateTime = "22.12.22 - 10:00";
-    }
-
-    public Flight(int id, int flightNumber, double price, String creationDate, String departure, String arrival, String departureDateTime, String arrivalDateTime) {
-        this.id = id;
-        this.flightNumber = flightNumber;
-        this.price = price;
-        this.creationDate = creationDate;
-        this.departure = departure;
-        this.arrival = arrival;
-        this.departureDateTime = departureDateTime;
-        this.arrivalDateTime = arrivalDateTime;
     }
 
     @Override
